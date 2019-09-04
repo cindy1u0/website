5c71ce030346e64ff9bc44d7b2ba4889d117e66a
cindy1u0 <luo.xiny@husky.neu.edu>
2019-09-03 22:27:03 -0400

    html

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,4 +1,4 @@
 document.getElementById("navMenu").innerHTML =
   '<a href="photo.html">Photography</a>' +
-  '<a href="projects">Project</a>' +
+  '<a href="project.html">Project</a>' +
   '<a href="index.html">About</a>';
